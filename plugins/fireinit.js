01cdd7919a283fdb2f7268e38a10a61766ec2a00
Jesper Paulsen <37295027+Jesperpaulsen@users.noreply.github.com>
2019-05-14 19:26:10 +0200

    Update fireinit.js

--- plugins/fireinit.js
+++ plugins/fireinit.js
@@ -5,12 +5,12 @@ import 'firebase/database'
 import Vue from 'vue'
 
 let config = {
-  apiKey: 'AIzaSyA6qBEDMWc2cJ5Y7xDKKNWiK3XDC9FbaJg',
-  authDomain: 'pu30-5b0f9.firebaseapp.com',
-  databaseURL: 'https://pu30-5b0f9.firebaseio.com',
-  projectId: 'pu30-5b0f9',
-  storageBucket: 'pu30-5b0f9.appspot.com',
-  messagingSenderId: '607415686100'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 }
 
 if (!firebase.apps.length) {
